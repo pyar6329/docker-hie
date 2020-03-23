00e7b5eb00a4be2e8186c02034166641bf6b55e4
Tsukasa Arima <pyar6329@gmail.com>
2020-03-24 05:30:50 +0900

    move git credentials to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+GITCONFIG=$(cat << 'EOF'
+[credential]
+  helper = store
+[url "https://github.com/"]
+  insteadOf = ssh://git@github.com/
+  insteadOf = git@github.com:
+EOF
+)
+
+if [ -n "${GITHUB_TOKEN}" ]; then
+  if ! [ -e "${HOME}/.git-credentials" ]; then
+    echo "https://${GITHUB_TOKEN}:@github.com" > ${HOME}/.git-credentials
+  fi
+  if ! [ -e "${HOME}/.gitconfig" ]; then
+    echo "${GITCONFIG}" > ${HOME}/.gitconfig
+  fi
+fi
+
 if ! [ -e "${STACK_ROOT}/global-project" ]; then
   WORKDIR="${PWD}"
   cd ${HOME}
